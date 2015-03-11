cf307bbb8a000cb0838592e6fc0ba020922d9067
farithjhg <HerasCantillo@HerasCant-PC>
2015-03-10 23:21:38 -0500

    New Beans

---
+++ src/main/java/com/currencyfair/data/entity/Country.java
@@ -0,0 +1,34 @@
+package com.currencyfair.data.entity;
+
+import java.io.Serializable;
+
+public class Country implements Serializable {
+    private static final long serialVersionUID = 1L;
+	private String countryCode; 
+	private int count;
+	
+	public Country() {
+	}
+
+	public Country(String countryCode, int count) {
+		this.countryCode = countryCode;
+		this.count = count;
+	}
+
+	public String getCountryCode() {
+		return countryCode;
+	}
+
+	public void setCountryCode(String countryCode) {
+		this.countryCode = countryCode;
+	}
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+	
+}
