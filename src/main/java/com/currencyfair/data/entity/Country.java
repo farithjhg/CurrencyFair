9611d35d80af1ad6128f2c90da9163361c47bccd
farithjhg <HerasCantillo@HerasCant-PC>
2015-03-10 23:22:10 -0500

    Bean

--- src/main/java/com/currencyfair/data/entity/Country.java
+++ src/main/java/com/currencyfair/data/entity/Country.java
@@ -2,6 +2,7 @@ package com.currencyfair.data.entity;
 
 import java.io.Serializable;
 
+
 public class Country implements Serializable {
     private static final long serialVersionUID = 1L;
 	private String countryCode; 
